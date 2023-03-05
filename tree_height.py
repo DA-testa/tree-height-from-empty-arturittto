87fd3813229df323f074dcb7e84cb43ea3866b1e
Artūrs Čubukovs <125797555+arturittto@users.noreply.github.com>
2023-03-05 23:10:42 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,6 +1,7 @@
 import sys
 import threading
 from typing import List
+
 def compute_tree_height(n_nodes: int, parents: List[int]) -> int:
     tree = [[] for i in range(n_nodes)]
     root = -1
@@ -19,15 +20,21 @@ def compute_tree_height(n_nodes: int, parents: List[int]) -> int:
                 queue.append(child)
         height += 1
     return height
+
+
 class TreeNode:
     def __init__(self, value):
         self.value = value
         self.children = []
+
+
 def compute_tree_height_recursive(node):
     if not node.children:
         return 1
     heights = [compute_tree_height_recursive(child) for child in node.children]
     return max(heights) + 1
+
+
 def main():
     input_type = input("")
     n_nodes = 0
@@ -42,8 +49,11 @@ def main():
         with open(f"./test/{file_path}", "r") as file:
             n_nodes = int(file.readline())
             parents = list(map(int, file.readline().split()))
+
     height = compute_tree_height(n_nodes, parents)
     print(height)
+
+
 sys.setrecursionlimit(10**7)
 threading.stack_size(2**27)
 threading.Thread(target=main).start()
