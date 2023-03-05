7585fbcf748d5852a461ba0b9e849a94454f4b76
Artūrs Čubukovs <125797555+arturittto@users.noreply.github.com>
2023-03-06 00:30:26 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,38 +1,51 @@
+import sys
+import threading
+import numpy as np
+
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
 def compute_height(n, parents):
-    tree = [[] for i in range(n)]
-    root = -1
-    for i in range(n):
-        if parents[i] == -1:
-            root = i
+    root_node = None
+    nodes = [TreeNode(i) for i in range(n)]
+    for i, parent in enumerate(parents):
+        if parent == -1:
+            root_node = nodes[i]
         else:
-            tree[parents[i]].append(i)
+            nodes[parent].children.append(nodes[i])
+
+    queue = [root_node]
     height = 0
-    queue = [root]
-    while len(queue) > 0:
+    while queue:
         level_size = len(queue)
         for i in range(level_size):
             node = queue.pop(0)
-            for child in tree[node]:
+            for child in node.children:
                 queue.append(child)
         height += 1
     return height
 
-
 def main():
-    text = input()
-    if text.startswith('I'):
-        n = int(input())
-        parents = list(map(int, input().split()))
+    input_type = input("")
+    n = 0
+    parents = []
+    if "I" in input_type:
+        n = int(input(""))
+        parents = list(map(int, input("").split()))
         print(compute_height(n, parents))
-    elif text.startswith('F'):
-        file = "./test/05"
-        with open(file) as f:
-            n = int(f.readline().strip())
-            parents = list(map(int, f.readline().strip().split()))
-            print(compute_height(n, parents))
     else:
-        print("Invalid input")
-#1
+        file_path = input("")
+        while "a" in file_path:
+            file_path = input("")
+        with open(f"./test/{file_path}", "r") as file:
+            n = int(file.readline())
+            parents = list(map(int, file.readline().split()))
 
-if __name__ == "__main__":
-    main()
+        height = compute_height(n, parents)
+        print(height)
+
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
+threading.Thread(target=main).start()
